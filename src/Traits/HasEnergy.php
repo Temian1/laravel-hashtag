bc89548c03efe31d4d09a9ceb7a90e8dc07851a9
Temian1 <39234980+Temian1@users.noreply.github.com>
2022-06-13 12:07:30 +0100

    Add files via upload

---
+++ src/Traits/HasEnergy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Hacklabs\Trends\Traits;
+
+use Illuminate\Http\Request;
+use Hacklabs\Trends\Jobs\EnergyDecay;
+use Hacklabs\Trends\Models\Energy;
+use Illuminate\Support\Facades\DB;
+
+trait HasEnergy 
+{
+    public function addEnergy($amount = 1) {
+        if(!$this->energy) {
+            $this->createEnergy();
+        }
+        $entity = $this->fresh();
+        if(!in_array(request()->ip(), config('trends.ip_blacklist'))) {
+            $this->energy()->update([
+                'amount' => $entity->energy->amount += $amount
+            ]);
+            EnergyDecay::dispatch($entity, 0.25)->delay(now()->addHours(config('trends.energy_decay')));
+            EnergyDecay::dispatch($entity, 0.45)->delay(now()->addHours(config('trends.energy_decay') * 2));
+            EnergyDecay::dispatch($entity, 0.30)->delay(now()->addHours(config('trends.energy_decay') * 3));
+        }
+    }
+
+    public function energy() {
+        return $this->morphOne(Energy::class, 'energisable');
+    }
+
+    public function getEnergyAmountAttribute() {
+        return ($this->energy) ? (float) $this->energy->amount : 0;
+    }
+
+    public function decayEnergy($amount) {
+        $this->energy()->update([
+            'amount' => $this->energy->amount -= $amount
+        ]);
+    }
+
+    public function getEntityName() {
+        return str_slug(get_class($this).' '.$this->id);
+    }
+
+    public function createEnergy() {
+        return $this->energy()->create([
+            'amount' => 0
+        ]);
+    }
+}
